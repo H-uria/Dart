f1cb1aeb6d266c9c0d296179ed47d35b3f7ae0ce
H-uria <151648915+H-uria@users.noreply.github.com>
2024-03-18 00:52:24 +0300

    Add files via upload

---
+++ Hackathon.assignment..dart
@@ -0,0 +1,8 @@
+void main() {
+  String name = "Dennis Huria";
+  int age = 23;
+  String school = "Moi University";
+  String hobby = "solving modern day problems using code";
+  
+  print("My name is $name. I am $age years old. I attend $school, and my hobby is $hobby.");
+}
---
+++ Hackathon.assignment.2.dart
@@ -0,0 +1,18 @@
+void main() {
+  // usage of mathematical operations
+  int resultAdd = add(10, 5);
+  int resultSubtract = subtract(20, 7);
+  
+  print("Result of addition: $resultAdd");
+  print("Result of subtraction: $resultSubtract");
+}
+
+// Function to perform addition
+int add(int a, int b) {
+  return a + b;
+}
+
+// Function to perform subtraction
+int subtract(int a, int b) {
+  return a - b;
+}
